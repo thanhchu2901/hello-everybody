c5d37019da497a4afed358c1919cb4ac78bcfaf9
thanhchu2901 <85271773+thanhchu2901@users.noreply.github.com>
2021-06-03 23:05:12 +0700

    Update config.js

--- config.js
+++ config.js
@@ -13,5 +13,5 @@ const CONFIG = {
     mess: 'Em hiểu chị mà 🥰. cảm on chị nhiều nhiều 😘😘',
     messDesc: 'Mai e cho chị ăn đấm nhé chị Lê Phương đểu đểu .',
     btnAccept: 'Okiiiii lun <3',
-    messLink:'https://i.pinimg.com/originals/07/2c/c0/072cc00dc835ad99c0ad924bb7dede73.jpg ' 
+   'https://i.pinimg.com/originals/07/2c/c0/072cc00dc835ad99c0ad924bb7dede73.jpg ' 
 }
